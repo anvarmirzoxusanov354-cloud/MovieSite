864e63a728a6370639227a1760570b62d28ba9ad
Anvarmirzo Xusanov <anvarmirzoxusanov354@gmail.com>
2026-04-28 15:15:58 +0500

    feat: movie rating formatlangan va toFixed(1) qo'shilgan

--- src/components/MovieSite/MovieCard.jsx
+++ src/components/MovieSite/MovieCard.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const MovieCard = ({ movie }) => {
   if (!movie) return null;
 
+  const rating = movie.vote_average ? movie.vote_average.toFixed(1) : "Not Found"; 
+  
   return (
     <div className="movie">
       <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
         alt={movie.title}
       />
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <span className="orange">{movie.vote_average}</span>
+        <span className="orange">{rating}</span>
       </div>
       <span className="date">{movie.release_date}</span>
     </div>
